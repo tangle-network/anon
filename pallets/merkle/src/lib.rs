353fa8b425b583bf40ca9e39a834d82d363b9aba
Filip <lazoviccorp@gmail.com>
2020-11-24 15:38:54 +0100

    spaces -> tabs

--- pallets/merkle/src/lib.rs
+++ pallets/merkle/src/lib.rs
@@ -29,8 +29,8 @@ use sp_std::prelude::*;
 
 /// The pallet's configuration trait.
 pub trait Trait: balances::Trait {
-    /// The overarching event type.
-    type Event: From<Event<Self>> + Into<<Self as frame_system::Trait>::Event>;
+	/// The overarching event type.
+	type Event: From<Event<Self>> + Into<<Self as frame_system::Trait>::Event>;
 }
 
 type GroupId = u32;
@@ -39,209 +39,209 @@ const MAX_DEPTH: u32 = 32;
 #[cfg_attr(feature = "std", derive(Debug))]
 #[derive(Encode, Decode, PartialEq)]
 pub struct GroupTree<T: Trait> {
-    pub fee: T::Balance,
-    pub leaf_count: u32,
-    pub max_leaves: u32,
-    pub root_hash: Data,
-    pub edge_nodes: Vec<Data>,
+	pub fee: T::Balance,
+	pub leaf_count: u32,
+	pub max_leaves: u32,
+	pub root_hash: Data,
+	pub edge_nodes: Vec<Data>,
 }
 
 impl<T: Trait> GroupTree<T> {
-    pub fn new(fee: T::Balance, depth: u32) -> Self {
-        Self {
-            fee,
-            root_hash: Data::zero(),
-            leaf_count: 0,
-            max_leaves: u32::MAX >> (MAX_DEPTH - depth),
-            edge_nodes: vec![Data::zero(); depth as usize],
-        }
-    }
+	pub fn new(fee: T::Balance, depth: u32) -> Self {
+		Self {
+			fee,
+			root_hash: Data::zero(),
+			leaf_count: 0,
+			max_leaves: u32::MAX >> (MAX_DEPTH - depth),
+			edge_nodes: vec![Data::zero(); depth as usize],
+		}
+	}
 }
 
 // This pallet's storage items.
 decl_storage! {
-    trait Store for Module<T: Trait> as MerkleGroups {
-        pub Groups get(fn groups): map hasher(blake2_128_concat) GroupId => Option<GroupTree<T>>;
-        pub UsedNullifiers get(fn used_nullifiers): map hasher(blake2_128_concat) Data => bool;
-    }
+	trait Store for Module<T: Trait> as MerkleGroups {
+		pub Groups get(fn groups): map hasher(blake2_128_concat) GroupId => Option<GroupTree<T>>;
+		pub UsedNullifiers get(fn used_nullifiers): map hasher(blake2_128_concat) Data => bool;
+	}
 }
 
 // The pallet's events
 decl_event!(
-    pub enum Event<T>
-    where
-        AccountId = <T as frame_system::Trait>::AccountId,
-    {
-        NewMember(u32, AccountId, Data),
-    }
+	pub enum Event<T>
+	where
+		AccountId = <T as frame_system::Trait>::AccountId,
+	{
+		NewMember(u32, AccountId, Data),
+	}
 );
 
 // The pallet's errors
 decl_error! {
-    pub enum Error for Module<T: Trait> {
-        /// Value was None
-        NoneValue,
-        ///
-        IncorrectNumOfPubKeys,
-        ///
-        ChallengeMismatch,
-        ///
-        BadPoint,
-    }
+	pub enum Error for Module<T: Trait> {
+		/// Value was None
+		NoneValue,
+		///
+		IncorrectNumOfPubKeys,
+		///
+		ChallengeMismatch,
+		///
+		BadPoint,
+	}
 }
 
 // The pallet's dispatchable functions.
 decl_module! {
-    /// The module declaration.
-    pub struct Module<T: Trait> for enum Call where origin: T::Origin {
-        type Error = Error<T>;
+	/// The module declaration.
+	pub struct Module<T: Trait> for enum Call where origin: T::Origin {
+		type Error = Error<T>;
 
-        fn deposit_event() = default;
+		fn deposit_event() = default;
 
-        #[weight = 0]
-        pub fn add_member(origin, group_id: u32, data: Data) -> dispatch::DispatchResult {
-            // Check it was signed and get the signer. See also: ensure_root and ensure_none
-            let who = ensure_signed(origin)?;
-            let mut tree = <Groups<T>>::get(group_id).ok_or("Group doesn't exist").unwrap();
-            ensure!(tree.leaf_count < tree.max_leaves, "Exceeded maximum tree depth.");
+		#[weight = 0]
+		pub fn add_member(origin, group_id: u32, data: Data) -> dispatch::DispatchResult {
+			// Check it was signed and get the signer. See also: ensure_root and ensure_none
+			let who = ensure_signed(origin)?;
+			let mut tree = <Groups<T>>::get(group_id).ok_or("Group doesn't exist").unwrap();
+			ensure!(tree.leaf_count < tree.max_leaves, "Exceeded maximum tree depth.");
 
-            let mut edge_index = tree.leaf_count;
-            let mut pair_hash = data;
-            // Update the tree
-            for i in 0..tree.edge_nodes.len() {
-                if edge_index % 2 == 0 {
-                    tree.edge_nodes[i] = pair_hash;
-                }
+			let mut edge_index = tree.leaf_count;
+			let mut pair_hash = data;
+			// Update the tree
+			for i in 0..tree.edge_nodes.len() {
+				if edge_index % 2 == 0 {
+					tree.edge_nodes[i] = pair_hash;
+				}
 
-                let hash = tree.edge_nodes[i];
-                pair_hash = Data::hash_mimc(hash, pair_hash);
+				let hash = tree.edge_nodes[i];
+				pair_hash = Data::hash_mimc(hash, pair_hash);
 
-                edge_index /= 2;
-            }
+				edge_index /= 2;
+			}
 
-            tree.leaf_count += 1;
-            tree.root_hash = pair_hash;
+			tree.leaf_count += 1;
+			tree.root_hash = pair_hash;
 
-            <Groups<T>>::insert(group_id, tree);
+			<Groups<T>>::insert(group_id, tree);
 
-            // Raising the New Member event for the client to build a tree locally
-            Self::deposit_event(RawEvent::NewMember(group_id, who, data));
-            Ok(())
-        }
+			// Raising the New Member event for the client to build a tree locally
+			Self::deposit_event(RawEvent::NewMember(group_id, who, data));
+			Ok(())
+		}
 
-        #[weight = 0]
-        pub fn verify(origin, group_id: u32, leaf: Data, path: Vec<(bool, Data)>) -> dispatch::DispatchResult {
-            let tree = <Groups<T>>::get(group_id)
-                .ok_or("Invalid group id.")
-                .unwrap();
-            ensure!(tree.edge_nodes.len() == path.len(), "Invalid path length.");
-            let mut hash = leaf;
-            for (is_right, node) in path {
-                hash = match is_right {
-                    true => Data::hash_mimc(hash, node),
-                    false => Data::hash_mimc(node, hash),
-                }
-            }
+		#[weight = 0]
+		pub fn verify(origin, group_id: u32, leaf: Data, path: Vec<(bool, Data)>) -> dispatch::DispatchResult {
+			let tree = <Groups<T>>::get(group_id)
+				.ok_or("Invalid group id.")
+				.unwrap();
+			ensure!(tree.edge_nodes.len() == path.len(), "Invalid path length.");
+			let mut hash = leaf;
+			for (is_right, node) in path {
+				hash = match is_right {
+					true => Data::hash_mimc(hash, node),
+					false => Data::hash_mimc(node, hash),
+				}
+			}
 
-            ensure!(hash == tree.root_hash, "Invalid proof of membership.");
-            Ok(())
-        }
+			ensure!(hash == tree.root_hash, "Invalid proof of membership.");
+			Ok(())
+		}
 
-        #[weight = 0]
-        pub fn verify_zk_membership_proof(
-            origin,
-            group_id: u32,
-            leaf_com: Commitment,
-            path: Vec<(Commitment, Commitment)>,
-            s_com: Commitment,
-            nullifier: Data,
-            proof_bytes: Vec<u8>
-        ) -> dispatch::DispatchResult {
-            // Ensure that nullifier is not used
-            ensure!(!UsedNullifiers::get(nullifier), "Nullifier already used.");
-            let tree = <Groups<T>>::get(group_id)
-                .ok_or("Invalid group id.")
-                .unwrap();
-            ensure!(tree.edge_nodes.len() == path.len(), "Invalid path length.");
+		#[weight = 0]
+		pub fn verify_zk_membership_proof(
+			origin,
+			group_id: u32,
+			leaf_com: Commitment,
+			path: Vec<(Commitment, Commitment)>,
+			s_com: Commitment,
+			nullifier: Data,
+			proof_bytes: Vec<u8>
+		) -> dispatch::DispatchResult {
+			// Ensure that nullifier is not used
+			ensure!(!UsedNullifiers::get(nullifier), "Nullifier already used.");
+			let tree = <Groups<T>>::get(group_id)
+				.ok_or("Invalid group id.")
+				.unwrap();
+			ensure!(tree.edge_nodes.len() == path.len(), "Invalid path length.");
 
-            let pc_gens = PedersenGens::default();
-            let bp_gens = BulletproofGens::new(2048, 1);
+			let pc_gens = PedersenGens::default();
+			let bp_gens = BulletproofGens::new(2048, 1);
 
-            let mut verifier_transcript = Transcript::new(b"zk_membership_proof");
-            let mut verifier = Verifier::new(&mut verifier_transcript);
+			let mut verifier_transcript = Transcript::new(b"zk_membership_proof");
+			let mut verifier = Verifier::new(&mut verifier_transcript);
 
-            let var_leaf = verifier.commit(leaf_com.0);
-            let var_s = verifier.commit(s_com.0);
-            let leaf_lc = Data::constrain_mimc(&mut verifier, var_s.into(), nullifier.0.into());
-            // Commited leaf value should be the same as calculated
-            verifier.constrain(leaf_lc - var_leaf);
+			let var_leaf = verifier.commit(leaf_com.0);
+			let var_s = verifier.commit(s_com.0);
+			let leaf_lc = Data::constrain_mimc(&mut verifier, var_s.into(), nullifier.0.into());
+			// Commited leaf value should be the same as calculated
+			verifier.constrain(leaf_lc - var_leaf);
 
-            // Check of path proof is correct
-            let mut hash: LinearCombination = var_leaf.into();
-            for (bit, pair) in path {
-                // e.g. If bit is 0 that means pair is on the left side
-                // var_bit = 0
-                let var_bit = verifier.commit(bit.0);
-                let var_pair = verifier.commit(pair.0);
+			// Check of path proof is correct
+			let mut hash: LinearCombination = var_leaf.into();
+			for (bit, pair) in path {
+				// e.g. If bit is 0 that means pair is on the left side
+				// var_bit = 0
+				let var_bit = verifier.commit(bit.0);
+				let var_pair = verifier.commit(pair.0);
 
-                // side = 1 - 0 = 1
-                let side: LinearCombination = Variable::One() - var_bit;
+				// side = 1 - 0 = 1
+				let side: LinearCombination = Variable::One() - var_bit;
 
-                // left1 = 0 * hash = 0
-                let (_, _, left1) = verifier.multiply(var_bit.into(), hash.clone());
-                // left2 = 1 * pair = pair
-                let (_, _, left2) = verifier.multiply(side.clone(), var_pair.into());
-                // left = 0 + pair = pair
-                let left = left1 + left2;
+				// left1 = 0 * hash = 0
+				let (_, _, left1) = verifier.multiply(var_bit.into(), hash.clone());
+				// left2 = 1 * pair = pair
+				let (_, _, left2) = verifier.multiply(side.clone(), var_pair.into());
+				// left = 0 + pair = pair
+				let left = left1 + left2;
 
-                // right1 = 1 * hash = hash
-                let (_, _, right1) = verifier.multiply(side, hash);
-                // right2 = 0 * pair = 0
-                let (_, _, right2) = verifier.multiply(var_bit.into(), var_pair.into());
-                // right = hash + 0 = hash
-                let right = right1 + right2;
+				// right1 = 1 * hash = hash
+				let (_, _, right1) = verifier.multiply(side, hash);
+				// right2 = 0 * pair = 0
+				let (_, _, right2) = verifier.multiply(var_bit.into(), var_pair.into());
+				// right = hash + 0 = hash
+				let right = right1 + right2;
 
-                hash = Data::constrain_mimc(&mut verifier, left, right);
-            }
-            // Commited path evaluate to correct root
-            verifier.constrain(hash - tree.root_hash.0);
+				hash = Data::constrain_mimc(&mut verifier, left, right);
+			}
+			// Commited path evaluate to correct root
+			verifier.constrain(hash - tree.root_hash.0);
 
-            let proof = R1CSProof::from_bytes(&proof_bytes);
-            ensure!(proof.is_ok(), "Invalid proof bytes.");
-            let proof = proof.unwrap();
+			let proof = R1CSProof::from_bytes(&proof_bytes);
+			ensure!(proof.is_ok(), "Invalid proof bytes.");
+			let proof = proof.unwrap();
 
-            let mut rng = OsRng {};
-            // Final verification
-            let res = verifier.verify_with_rng(&proof, &pc_gens, &bp_gens, &mut rng);
-            ensure!(res.is_ok(), "Invalid proof of membership or leaf creation.");
+			let mut rng = OsRng {};
+			// Final verification
+			let res = verifier.verify_with_rng(&proof, &pc_gens, &bp_gens, &mut rng);
+			ensure!(res.is_ok(), "Invalid proof of membership or leaf creation.");
 
-            // Set nullifier as used
-            UsedNullifiers::insert(nullifier, true);
+			// Set nullifier as used
+			UsedNullifiers::insert(nullifier, true);
 
-            Ok(())
-        }
+			Ok(())
+		}
 
-        #[weight = 0]
-        pub fn create_group(origin, group_id: GroupId, _fee: Option<T::Balance>, _depth: Option<u32>) -> dispatch::DispatchResult {
-            let _sender = ensure_signed(origin)?;
-            ensure!(!<Groups<T>>::contains_key(group_id), "Group already exists.");
+		#[weight = 0]
+		pub fn create_group(origin, group_id: GroupId, _fee: Option<T::Balance>, _depth: Option<u32>) -> dispatch::DispatchResult {
+			let _sender = ensure_signed(origin)?;
+			ensure!(!<Groups<T>>::contains_key(group_id), "Group already exists.");
 
-            let fee = match _fee {
-                Some(f) => f,
-                None => Zero::zero(),
-            };
+			let fee = match _fee {
+				Some(f) => f,
+				None => Zero::zero(),
+			};
 
-            let depth = match _depth {
-                Some(d) => d,
-                None => MAX_DEPTH
-            };
+			let depth = match _depth {
+				Some(d) => d,
+				None => MAX_DEPTH
+			};
 
-            ensure!(depth <= MAX_DEPTH && depth > 0, "Invalid tree depth.");
+			ensure!(depth <= MAX_DEPTH && depth > 0, "Invalid tree depth.");
 
-            let mtree = GroupTree::<T>::new(fee, depth);
-            <Groups<T>>::insert(group_id, mtree);
+			let mtree = GroupTree::<T>::new(fee, depth);
+			<Groups<T>>::insert(group_id, mtree);
 
-            Ok(())
-        }
-    }
+			Ok(())
+		}
+	}
 }
