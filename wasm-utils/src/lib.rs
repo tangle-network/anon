5bff76d75d5fed34e645ef0c21c8413e8dd484f4
Filip <lazoviccorp@gmail.com>
2020-12-21 18:33:18 +0100

    Added prove and verify

--- wasm-utils/src/lib.rs
+++ wasm-utils/src/lib.rs
@@ -13,9 +13,15 @@ extern "C" {
 	fn getItem(key: &str);
 }
 
-#[wasm_bindgen]
-pub fn greet() {
-	log("Hello from rust");
+pub fn set_panic_hook() {
+	// When the `console_error_panic_hook` feature is enabled, we can call the
+	// `set_panic_hook` function at least once during initialization, and then
+	// we will get better error messages if our code ever panics.
+	//
+	// For more details see
+	// https://github.com/rustwasm/console_error_panic_hook#readme
+	#[cfg(feature = "console_error_panic_hook")]
+	console_error_panic_hook::set_once();
 }
 
 pub struct TreeState {
@@ -27,7 +33,7 @@ pub struct TreeState {
 pub struct MerkleClient {
 	curr_root: Data,
 	states: HashMap<Data, TreeState>,
-	leaves: Vec<Data>,
+	leaf_indices: HashMap<Data, usize>,
 	levels: Vec<Vec<Data>>,
 	hasher: Poseidon,
 	max_leaves: u32,
@@ -48,7 +54,7 @@ impl MerkleClient {
 		Self {
 			curr_root: init_root,
 			states: init_states,
-			leaves: Vec::new(),
+			leaf_indices: HashMap::new(),
 			levels: vec![Vec::new(); num_levels],
 			hasher: Poseidon::new(4),
 			max_leaves: u32::MAX >> (max_levels - num_levels),
@@ -66,7 +72,7 @@ impl MerkleClient {
 impl MerkleClient {
 	pub fn add_leaf(&mut self, leaf: [u8; 32]) {
 		assert!(
-			self.leaves.len() < self.max_leaves as usize,
+			self.levels[0].len() < self.max_leaves as usize,
 			"Tree is already full!"
 		);
 		let curr_state = self.states.get(&self.curr_root).unwrap();
@@ -83,18 +89,72 @@ impl MerkleClient {
 			if edge_index % 2 == 0 {
 				new_state.edge_nodes[i] = pair_hash;
 			}
+			let level = self.levels.get_mut(i).unwrap();
+			level.push(pair_hash);
 
 			let hash = new_state.edge_nodes[i];
 			pair_hash = Data::hash(hash, pair_hash, &self.hasher);
-			let level = self.levels.get_mut(i).unwrap();
-			level.push(pair_hash);
 
 			edge_index /= 2;
 		}
 
 		self.curr_root = pair_hash;
-		self.leaves.push(data);
 		self.states.insert(pair_hash, new_state);
+	}
+
+	pub fn prove(&self, root_bytes: [u8; 32], leaf_bytes: [u8; 32]) -> Vec<(bool, [u8; 32])> {
+		let root = Data::from(root_bytes);
+		let leaf = Data::from(leaf_bytes);
+		assert!(self.states.contains_key(&root), "Root not found!");
+		assert!(self.leaf_indices.contains_key(&leaf), "Leaf not found!");
+
+		let state = self.states.get(&root).unwrap();
+
+		let mut node_index = self.leaf_indices.get(&leaf).cloned().unwrap();
+		let mut path = Vec::new();
+		for (i, level) in self.levels.iter().enumerate() {
+			let res = if node_index % 2 == 0 {
+				let node = if node_index == 0 {
+					level[0]
+				} else {
+					level[node_index - 1]
+				};
+				(false, node.0.to_bytes())
+			} else {
+				let node = if node_index == level.len() - 1 {
+					state.edge_nodes[i]
+				} else {
+					level[node_index + 1]
+				};
+				(true, node.0.to_bytes())
+			};
+
+			path.push(res);
+			node_index /= 2;
+		}
+
+		path
+	}
+
+	pub fn verify(
+		&self,
+		root_bytes: [u8; 32],
+		leaf_bytes: [u8; 32],
+		path: Vec<(bool, [u8; 32])>,
+	) -> bool {
+		let root = Data::from(root_bytes);
+		let mut hash = Data::from(leaf_bytes);
+		for (right, bytes) in path {
+			let pair = Data::from(bytes);
+
+			hash = if right {
+				Data::hash(hash, pair, &self.hasher)
+			} else {
+				Data::hash(pair, hash, &self.hasher)
+			}
+		}
+
+		root == hash
 	}
 }
 
