7aa3108f96b6a6016d06cb0ade4b9962f59efd64
Filip <lazoviccorp@gmail.com>
2021-01-05 16:56:17 +0100

    bumbped version, using npm package

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -2,13 +2,14 @@ const isServer = () => typeof window === 'undefined';
 
 function Index() {
 	if (!isServer()) {
-		import("../merkle-client").then(wasm => {
+		import("mixer-cl").then(wasm => {
 			let cl = wasm.Mixer.new([["EDG", 0, 2]]);
 			let note = cl.generate_note("EDG", 0);
 			console.log(note, note.length);
 			let leaf = cl.save_note(note);
 			console.log(leaf);
 			cl.save_note_to_storage(note);
+			cl.load_notes_from_storage();
 
 			cl.add_leaves("EDG", 0, [leaf]);
 			let root = cl.get_root("EDG", 0);
