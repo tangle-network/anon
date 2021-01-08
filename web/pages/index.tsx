2f2fb3e0f22a7a2d9eb8af3675035858edc654b0
Filip <lazoviccorp@gmail.com>
2021-01-08 22:45:16 +0100

    version bump

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -4,13 +4,14 @@ function Index() {
 	if (!isServer()) {
 		import("../mixer-client").then(wasm => {
 			let cl = wasm.Mixer.new([["EDG", 0, 32]]);
-			let note = cl.generate_note("EDG", 0);
+			let note = cl.generate_note("EDG", 0, 123);
 			console.log(note, note.length);
 			let data = cl.save_note(note);
 			let leaf = data.get("leaf");
 			let asset = data.get("asset");
 			let id = data.get("id");
-			console.log(leaf, asset, id);
+			let block_number = data.get("block_number");
+			console.log(leaf, asset, id, block_number);
 			cl.save_note_to_storage(note);
 			cl.load_notes_from_storage();
 
