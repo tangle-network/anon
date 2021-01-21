c9436a2f36ef649d81698977898b325a3b22834c
Drew Stone <drewstone329@gmail.com>
2021-01-20 19:38:48 -0500

    Fix dups

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -8,7 +8,6 @@
 include!(concat!(env!("OUT_DIR"), "/wasm_binary.rs"));
 use pallet_grandpa::{fg_primitives, AuthorityId as GrandpaId, AuthorityList as GrandpaAuthorityList};
 pub use pallet_transaction_payment::{CurrencyAdapter, Multiplier, TargetedFeeAdjustment};
-use pallet_transaction_payment::{FeeDetails, RuntimeDispatchInfo};
 use sp_api::impl_runtime_apis;
 use sp_consensus_aura::sr25519::AuthorityId as AuraId;
 use sp_core::{crypto::KeyTypeId, OpaqueMetadata};
