fd50a8ba01e243417d802bf349342174adf3fd88
Drew Stone <drewstone329@gmail.com>
2021-02-04 13:36:51 -0500

    Update CI test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 SCRIPTDIR=$PWD
+
+# test each pallets tests
 for d in $(ls -d ./pallets/*/) ; do
     cd "$SCRIPTDIR/$d" && WASM_BUILD_TOOLCHAIN=nightly-2020-10-05 cargo test
 done
 
+# test wasm utils
 cd "$SCRIPTDIR/wasm-utils" && ./test.sh
+
+# test gadget tests
+cd "$SCRIPTDIR/gadgets" && WASM_BUILD_TOOLCHAIN=nightly-2020-10-05 cargo test
